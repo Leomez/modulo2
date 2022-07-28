d382b575004f950a5c67ca9c1e92d94c25015ebe
Leomez <leoariel17@gmail.com>
2022-07-28 00:45:35 -0300

    v1.2

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -42,13 +42,26 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+   matchFunction = (ele) => ele.id === selector.slice(1)
   } else if (selectorType === "class") {
-    
+    // matchFunction = (ele) => ele.classList.contains(selector(slice(1)))    no me funca
+    matchFunction = function (ele) {
+      let e = ele.classList;
+      for (let i = 0; i < e.length; i++) {
+        if (e[i] === selector.slice(1)) {
+          return true;
+        } 
+      }
+      return false;        
+    }
   } else if (selectorType === "tag.class") {
+    matchFunction = function (ele) {
+      let [tag, classN] = selector.slice('.');
+      return (matchFunctionMaker(tag)(ele) && matchFunctionMaker(`.${classN}`)(ele));
+    }
     
   } else if (selectorType === "tag") {
-    
+    matchFunction = (ele) => ele.tagName.toUpperCase() === selector.toUpperCase();
   }
   return matchFunction;
 };
