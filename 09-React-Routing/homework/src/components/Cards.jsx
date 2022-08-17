907a0b503b7ddedc045190c1605679326b173156
Leonardo <leoariel17@gmail.com>
2022-08-17 16:02:50 -0300

    rutas sin terminar

--- 09-React-Routing/homework/src/components/Cards.jsx
+++ 09-React-Routing/homework/src/components/Cards.jsx
@@ -6,6 +6,7 @@ export default function Cards({ cities, onClose }) {
   if (cities) {
     console.log(cities);
     return (
+      
       <div className={s.cards}>
         {cities.map(c => {
           console.log(c.id);
