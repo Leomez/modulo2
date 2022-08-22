110ebee4d238d9a76565177579882db76c6530b0
Leonardo <leoariel17@gmail.com>
2022-08-22 11:51:50 -0300

    Form Terminado

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -5,7 +5,7 @@ export function validate(input){
   let errors = {};
   if (!input.username) {
     errors.username = 'Username is required'
-  } else if (!/\S+@\.\S+/.test(input.username)){
+  } else if (!/\S+@\S+\.\S+/.test(input.username)){
     errors.username = 'Username is invalid'
   }
   if (!input.password) {
@@ -13,45 +13,50 @@ export function validate(input){
   } else if (!/(?=.*[0-9])/.test(input.password)) {
     errors.password = 'Password is invalid'
   }
+  // console.log(errors);  
    
   return errors
   
 }
 
-const [errors, setErrors] = useState({});
+
 
 export default function  Form() {
   const [input, setInput] = useState({
     username: '',
     password: ''
-  });  
+  });
+
+  const [errors, setErrors] = useState({});
 
   const handleInputChange = function (e) {
     setInput({
       ...input,
       [e.target.name]: e.target.value
-    })
+    });
     setErrors(validate({
       ...input,
       [e.target.name]: e.target.value
-    }));
+    }));    
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(input.username);
-    console.log(input.password);
+    e.preventDefault();    
+    // console.log(input.username);
+    // console.log(input.password);    
   }
 
   return (
     <form onSubmit={handleSubmit} action="">
       <div>
         <label htmlFor="">Username:</label>
-        <input type="text" name='username' onChange={handleInputChange} value={input.username} />
+        <input className={errors.username && 'danger'} type="text" name='username' onChange={handleInputChange} value={input.username} />
+        {errors.username && (<p className="danger">{errors.username}</p>)}
       </div>
       <div>
         <label htmlFor="">Password:</label>
-        <input type="password" name='password' onChange={handleInputChange} value={input.password}/>
+        <input className={errors.password && 'danger'} type="password" name='password' onChange={handleInputChange} value={input.password}/>
+        {errors.password && (<p className="danger">{errors.password}</p>)}
       </div>
       <input type="submit" />
 
