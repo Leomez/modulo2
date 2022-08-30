9a7f61da811697d2f70ae7b495ba495614f98391
Leonardo <leoariel17@gmail.com>
2022-08-30 15:54:15 -0300

    commit definitivo del SearchBar

--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import s from './SearchBar.module.css';
 
-
-
-
 export default function SearchBar({ onSearch }) {
+  const [ciudad, setCiudad] = useState("");
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
       // console.log(document.getElementById('ciudad').value);      
-      onSearch(document.getElementById('ciudad').value);
+      onSearch(ciudad);
       
     }}>
       <input
         type="text"
         placeholder="Ciudad..."
         id="ciudad"
+        value={ciudad}
+        onChange={e => setCiudad(e.target.value)}
       />
       <input className={s.button} type="submit" value="Agregar" />
     </form>
